4eb5806dac71396e061b1cef65943acb1ba72736
Masashi Katsumata <wf9a5m75@gmail.com>
2026-06-27 08:46:24 +0900

    chore: bump ios-sdk-core dependency to 1.0.6

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/MapConductor/ios-sdk-core", from: "1.0.0"),
+        .package(url: "https://github.com/MapConductor/ios-sdk-core", from: "1.0.6"),
         .package(url: "https://github.com/googlemaps/ios-maps-sdk", from: "10.0.0"),
     ],
     targets: [
